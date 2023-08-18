bf98a7ee7f64a9d012f4657b327d186b87f79e5c
Bhupesh Kumbhar <bhupesh.kumbhar@gmail.com>
2023-08-18 15:34:55 +0530

    add file

---
+++ test.py
@@ -0,0 +1,11 @@
+from PyPDF import PdfReader, PdfWriter
+
+reader = PdfReader("big_file.pdf")
+writer = PdfWriter()
+
+for page in reader.pages:
+    page.compress_content_streams()  # This is CPU intensive!
+    writer.add_page(page)
+
+with open("small.pdf", "wb") as f:
+    writer.write(f)
